b1f78da1826f5b4e3683742d1e3b2f8885c52533
Mayur Khotele <mayurkhotele111@gmail.com>
2025-10-21 00:26:40 +0530

    change

--- src/app/api/admin/questions/route.ts
+++ src/app/api/admin/questions/route.ts
@@ -45,7 +45,7 @@ export async function POST(req: NextRequest) {
     data: {
       text,
       options,
-      correct,
+      correctAnswer: correct,
       examId: examId || undefined,
     },
   });
